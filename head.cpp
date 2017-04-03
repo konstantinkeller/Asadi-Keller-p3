f7cad0a889042ddea96f7da1d8f8f0f4ee975322
Konstantin Keller <konstantinkeller5@gmail.com>
2017-04-03 14:19:08 -0400

    Organized code in head and made it mostly-functional

--- head.cpp
+++ head.cpp
@@ -4,58 +4,111 @@
 #include <string>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <vector>
 
-using std::cout;
-using std::string;
+using namespace std;
+
+void print_lines_file(char *);
+void print_lines_stdin();
 
 int max_lines = 10;
 
-int main (int argc, char * argv[]) {
+int main(int argc, char * argv[]) {
     char * fname;
-    int fd;
+    vector<char *> fileargs;
 
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            if (argv[i][0] == '-') {
+                switch (argv[i][1]) {
+                    case 'n':
+                        max_lines = atoi(argv[i+1]);
+                }
+            } else if (argv[i-1][0] != '-') {
+                fname = argv[i];
+                fileargs.push_back(fname);
+            }
+        }
+    }
+
+    if (fileargs.size() == 0) {
+        print_lines_stdin();
+    } else {
+        for (char * file : fileargs) {
+            print_lines_file(file);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void print_lines_file(char * fname) {
+    int fd;
     int lines_left;
+
+    int bytes_to_write;
 
     const size_t BUFF_SIZE = 1024;
     char buffer [BUFF_SIZE];
     int n;
 
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            if (string(argv[i]) == "-n") {
-                max_lines = atoi(argv[i+1]);
-            } else {
-                fname = argv[i];
-                if ((fd = open(fname, O_RDONLY)) != -1) {
-                    lines_left = max_lines;
-                    n = 0;
-                    while (lines_left) {
-                        n = read(fd, buffer, BUFF_SIZE);
-                        if (n == 0) break;
-                        for (int i = 0; i < n; i++) {
-                            if (buffer[i] == '\n' && lines_left-- == 0) {
-                                off_t bytes_past_EOL = n - i;
-                                if (lseek (fd, -bytes_past_EOL, SEEK_CUR) < 0) {
-                                    //struct stat st;
-                                    //if (fstat (fd, &st) != 0 || S_ISREG (st.st_mode))
-                                        //elseek (fd, -bytes_past_EOL, SEEK_CUR, fname);
-                                }
-                                break;
-                            }
-                        }
-                        if (write(STDOUT_FILENO, buffer, n) == -1) {
-                            cout << "Error while writing file to stdout.";
-                            exit(EXIT_FAILURE);
-                        }
+    if ((fd = open(fname, O_RDONLY)) != -1) {
+        lines_left = max_lines;
+        n = 0;
+        while (lines_left > 0) {
+            n = read(fd, buffer, BUFF_SIZE);
+            if (n == 0) break;
+            for (int i = 0; i < n; i++) {
+                if (buffer[i] == '\n') {
+                    lines_left--;
+                    if (lines_left == 0) {
+                        bytes_to_write = i;
+                        break;
                     }
-                    close(fd);
-                } else {
-                    cout << "Error while opening file.";
-                    exit(EXIT_FAILURE);
                 }
+                bytes_to_write = n;
+            }
+            if (write(STDOUT_FILENO, buffer, bytes_to_write) == -1) {
+                cout << "Error while writing file to stdout.";
+                exit(EXIT_FAILURE);
             }
         }
+        cout << std::endl;
+        close(fd);
+    } else {
+        cout << "Error while opening file.";
+        exit(EXIT_FAILURE);
     }
+}
 
-    return EXIT_SUCCESS;
+void print_lines_stdin() {
+    int lines_left;
+
+    int bytes_to_write;
+
+    const size_t BUFF_SIZE = 1024;
+    char buffer [BUFF_SIZE];
+    int n;
+
+    lines_left = max_lines;
+    n = 0;
+    while (lines_left > 0) {
+        n = read(STDOUT_FILENO, buffer, BUFF_SIZE);
+        if (n == 0) break;
+        for (int i = 0; i < n; i++) {
+            if (buffer[i] == '\n') {
+                lines_left--;
+                if (lines_left == 0) {
+                    bytes_to_write = i;
+                    break;
+                }
+            }
+            bytes_to_write = n;
+        }
+        if (write(STDOUT_FILENO, buffer, bytes_to_write) == -1) {
+            cout << "Error while writing to stdout.";
+            exit(EXIT_FAILURE);
+        }
+    }
+    cout << std::endl;
 }
